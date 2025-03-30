e9e656fbaa41e40d916af85ded01c14cceb0a78d
sid2487 <sid454804@gmail.com>
2025-03-31 03:17:27 +0530

    learnt about file system in nodejs

--- NODEJS/todo/todo.js
+++ NODEJS/todo/todo.js
@@ -1,44 +1,76 @@
-const fs = require("fs");
+const fs = require('fs')
+const filePath = "./tasks.json"
 
-const filePath = "./task.json";
-
+// loading the task(3rd)
 const loadTask = () => {
     try {
-        const dataBuffer = fs.readFileSync(filePath);
-        const dataJSON = dataBuffer.toString();
-        return JSON.parse(dataJSON);
+        const dataBuffer = fs.readFileSync(filePath)
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
     } catch (error) {
-        return [];
+        return []
     }
-};
+}
 
+// addign save task(4th)
 const saveTasks = (tasks) => {
-    const dataJSON = JSON.stringify(tasks);
-    fs.writeFileSync(filePath, dataJSON);
-};
+    const dataJSON = JSON.stringify(tasks)
+    fs.writeFileSync(filePath, dataJSON)
+}
 
+// adding a task(2nd)
 const addTask = (task) => {
-    const tasks = loadTask();
-    tasks.push({task});
-    saveTasks(tasks);
-    console.log("Task added", task);
-};
+    const tasks = loadTask()
+    tasks.push({task})
+    saveTasks(tasks)
+    console.log("Task added", task); // (5th)
+    
+}
 
-const listTasks = () => {
-    const tasks = loadTask();
-    tasks.forEach((task, index) => console.log(`${index + 1} - ${task.task}`));
-};
+// list the task(6th)
+const listTask = () => {
+    const tasks = loadTask() // it will give an array
+    tasks.forEach((task, index) => {
+        console.log(`${index+1} - ${task.task}`)
+    })
+}
 
-const command = process.argv[2];
+// remove the task using filter(7th)-->> creates a new array 
+// const removeTask = (taskToRemove) => {
+//     const tasks = loadTask();
+//     const updatedTask = tasks.filter(task => task.task != taskToRemove); // remove the matching task and keep the unmatched task in a array named updatedTask.
+//     saveTasks(updatedTask);
+//     console.log(`Task removed: ${taskToRemove}`);
+    
+// }
+
+// remove the task using splice(7th)-->> modifies the existing array
+const removeTask = (taskToRemove) => {
+    let tasks = loadTask();
+
+    const index = tasks.findIndex(task => task.task === taskToRemove)
+
+    if(tasks != -1) {
+        tasks.splice(index, 1); 
+        saveTasks(tasks)
+        console.log(`Task removed: ${taskToRemove}`)
+    } else {
+        console.log(`Task not found: ${taskToRemove}`)
+    }
+}
+
+
+// to grab the things through command(1st)
+const command = process.argv[2]
 const argument = process.argv[3];
+
 
 if(command === "add") {
     addTask(argument);
 } else if(command === "list") {
-    listTasks();
+    listTask();
 } else if(command === "remove") {
-    removeTask(parseInt(argument));
+    removeTask(argument);
 } else {
-    console.log("Command not found");
-    
+    console.log("Command not found!");
 }
